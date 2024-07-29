93c91d2175fe5d991e5dca581a7aac0a7ff7441b
Elina <elina50505051@yandex.ru>
2024-07-29 19:41:35 +0300

    feat: add active card state

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Offer} from '../../mocks/types.ts';
-import {v4 as uuidv4} from 'uuid';
 import Card from '../card/card.tsx';
 
 interface CardListProps {
   offers: Offer[];
 }
 
-const CardList = ({ offers }: CardListProps): React.JSX.Element => (
-  <div className="cities__places-list places__list tabs__content">
-    {
-      offers?.map((offer) => (
-        <Card offer={offer} key={uuidv4()}/>
-      ))
-    }
-  </div>
-);
+const CardList = ({ offers }: CardListProps): React.JSX.Element => {
+  const [, setActiveCardID] = useState<string | null>(null);
+  const handleCardHover = (id: string | null): void => {
+    setActiveCardID(id);
+  };
+
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {
+        offers?.map((offer) => (
+          <Card offer={offer} handleCardHover={handleCardHover} key={offer.id}/>
+        ))
+      }
+    </div>
+  );
+};
 
 export default CardList;
