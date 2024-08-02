a16296f2f9d49b7eb8814b197dfbbdcbda6bbf73
Elina <elina50505051@yandex.ru>
2024-08-02 19:28:26 +0300

    feat: change mock data

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -9,13 +9,13 @@ const offers: OfferMock[] = [
     city: {
       name: 'Amsterdam',
       location: {
-        latitude: 52.35514938496378,
-        longitude: 4.673877537499948,
+        latitude: 52.3909553943508,
+        longitude: 4.85309666406198,
         zoom: 8
       }},
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 8
     },
     title: 'Beautiful & luxurious apartment at great location',
@@ -42,13 +42,13 @@ const offers: OfferMock[] = [
     city: {
       name: 'Amsterdam',
       location: {
-        latitude: 52.35514938496378,
-        longitude: 4.673877537499948,
+        latitude: 52.3609553943508,
+        longitude: 4.85309666406198,
         zoom: 8
       }},
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
       zoom: 8
     },
     title: 'Hotel at great location',
@@ -75,13 +75,13 @@ const offers: OfferMock[] = [
     city: {
       name: 'Amsterdam',
       location: {
-        latitude: 52.35514938496378,
-        longitude: 4.673877537499948,
+        latitude: 52.3909553943508,
+        longitude: 4.929309666406198,
         zoom: 8
       }},
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 8
     },
     title: 'Room at great location',
@@ -108,13 +108,13 @@ const offers: OfferMock[] = [
     city: {
       name: 'Cologne',
       location: {
-        latitude: 52.35514938496378,
-        longitude: 4.673877537499948,
+        latitude: 52.3809553943508,
+        longitude: 4.939309666406198,
         zoom: 8
       }},
     location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 8
     },
     title: 'Beautiful & luxurious house at great location',
