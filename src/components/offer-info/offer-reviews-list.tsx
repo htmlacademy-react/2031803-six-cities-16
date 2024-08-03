3af670596651969fa492d1d860e9af7d4bf0245b
Elina <elina50505051@yandex.ru>
2024-08-04 02:54:55 +0300

    refactor: lift form render logic to the parent component

--- src/components/offer-info/offer-reviews-list.tsx
+++ src/components/offer-info/offer-reviews-list.tsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import OfferForm from './offer-form.tsx';
 import OfferReview from './offer-review.tsx';
 import {Review, Reviews} from '../../mocks/types.ts';
 import reviewsMocks from '../../mocks/reviews.ts';
+import {AppContext} from '../app/app.tsx';
+import {AuthStatus} from '../app/types.ts';
 
 interface OfferReviewsListProps {
   offerID: keyof Reviews;
@@ -10,13 +12,14 @@ interface OfferReviewsListProps {
 
 const OfferReviewsList = ({ offerID }: OfferReviewsListProps): React.JSX.Element => {
   const [reviews] = useState(reviewsMocks[offerID]);
+  const { authStatus} = useContext(AppContext);
   return (
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
         {reviews.map((review: Review) => <OfferReview review={review} key={review.id}/>)}
       </ul>
-      <OfferForm/>
+      {authStatus === AuthStatus.Auth ? <OfferForm/> : null}
     </section>
   );
 };
