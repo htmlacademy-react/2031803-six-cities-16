961280db690179ddf19a60850cd8d213c6e24c38
Elina <elina50505051@yandex.ru>
2024-08-04 19:44:08 +0300

    feat: render 'other places in the neighbourhood' in Offer page

--- src/pages/offer.tsx
+++ src/pages/offer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import OfferInfo from '../components/offer-info/offer-info.tsx';
-import OffersNearList from '../components/offer-info/offers-near-list.tsx';
 import {useParams} from 'react-router-dom';
 
 const OfferPage = (): React.JSX.Element => {
@@ -8,15 +7,8 @@ const OfferPage = (): React.JSX.Element => {
   return (
     <main className="page__main page__main--offer">
       <OfferInfo offerID={offerID ?? ''}/>
-      <div className="container">
-        <section className="near-places places">
-          <h2 className="near-places__title">Other places in the neighbourhood</h2>
-          <OffersNearList/>
-        </section>
-      </div>
     </main>
   );
 };
 
 export default OfferPage;
-
