c38977bb69c446ca3c7bfd7e3c6b8541929ee7d1
Elina <elina50505051@yandex.ru>
2024-08-19 20:26:39 +0300

    feat: add sort options to redux state

---
+++ src/components/sorting-list/types.ts
@@ -0,0 +1,6 @@
+export enum SortOption {
+  Popular = 'Popular',
+  PriceAsc = 'Price: low to high',
+  PriceDesc = 'Price: high to low',
+  TopRated = 'Top rated first',
+}
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -6,6 +6,7 @@ import {CITIES} from '../../const.ts';
 import {useAppSelector} from '../../hooks/hooks.ts';
 import {selectCity} from '../../store/reducers/city/city.ts';
 import {selectCityOffers} from '../../store/reducers/offer/offer.ts';
+import SortingList from '../../components/sorting-list/sorting-list.tsx';
 
 const MainPage = (): React.JSX.Element => {
   const cityOffers = useAppSelector(selectCityOffers);
@@ -32,12 +33,7 @@ const MainPage = (): React.JSX.Element => {
                   <use xlinkHref="#icon-arrow-select"></use>
                 </svg>
               </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                <li className="places__option" tabIndex={0}>Price: low to high</li>
-                <li className="places__option" tabIndex={0}>Price: high to low</li>
-                <li className="places__option" tabIndex={0}>Top rated first</li>
-              </ul>
+              <SortingList/>
             </form>
             <CardList offers={cityOffers}/>
           </section>
